6b06bbbb8c6c2b00611d97e49ced928f07f9e49a
Tigran TIKSN Torosyan <personal@tiksn.com>
2025-07-31 20:19:28 -0500

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "fusionauth_api_key" {
+  description = "The API key for FusionAuth"
+  type        = string
+}
+
+variable "fusionauth_base_url" {
+  description = "The base URL of the FusionAuth instance"
+  type        = string
+}
